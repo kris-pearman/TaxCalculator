4148921857d14dd5ffd5687c1ca907173f39304a
kris-pearman <57414402+kris-pearman@users.noreply.github.com>
2025-01-12 14:34:07 +0000

    Initial commit

---
+++ TaxCalculatorAPITests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TaxCalculatorAPITests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
